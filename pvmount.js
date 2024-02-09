e0592ec01b39d696bb2de5374e08e55168488668
Sonny Lloyd <sonny.lloyd@hmcts.net>
2024-02-09 08:31:59 +0000

    trigger push

--- pvmount.js
+++ pvmount.js
@@ -45,82 +45,80 @@ if (!namespace || !environment) {
 console.log(`Mounting with Namespace: ${namespace}, Environment: ${environment} to folder location ${mount}`);
 
 async function mountSecrets(vaultName, nameSpace, outputDir = null) {
-    if (!outputDir) {
-      throw new Error('mount point is required');
-    }
-  
-    try {
-      // Create an Azure CLI credential
-      const credential = new AzureCliCredential();
-  
-      // Create a SecretClient
-      const vaultUrl = `https://${vaultName}.vault.azure.net/`;
-      const client = new SecretClient(vaultUrl, credential);
-  
-      // Get all secrets from the Key Vault
-      const secrets = client.listPropertiesOfSecrets();
-  
-      // Create the output directory if it doesn't exist
-      if (outputDir) {
-        fs.mkdirSync(path.join(outputDir, nameSpace), { recursive: true });
-      }
-  
-      // Loop through the secrets and store them in files
-      for await (const secretProperties of secrets) {
-        const secretName = secretProperties.name;
-        const secretValue = await client.getSecret(secretName);
-  
-        // Define the output file path
-        const filePath = outputDir ? path.join(outputDir, nameSpace, secretName) : secretName;
-  
-        // Write the secret value to the file
-        fs.writeFileSync(filePath, secretValue.value);
-  
-        console.log(`Secret '${secretName}' stored in '${filePath}'`);
-      }
-    } catch (error) {
-      if (error.name === 'CredentialUnavailableError') {
-        console.error("Azure CLI credential is not available. Please log in using 'az login'.");
-        program.help();
-      } else {
-        console.error(error);
-        program.help();
-      }
-    }
+  if (!outputDir) {
+    throw new Error('mount point is required');
   }
 
-  async function createSyntheticLink(folderName, actualPath) {
-    const commands = [
-      `sudo touch /etc/synthetic.conf`,
-      `sudo chmod 0666 /etc/synthetic.conf`,
-      `sudo printf "${folderName}\t${actualPath}\n" >> /etc/synthetic.conf`,
-      `sudo chmod 0644 /etc/synthetic.conf`,
-      `sudo chown root:wheel /etc/synthetic.conf`,
-      `/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util -t`,
-      `sudo rm /etc/synthetic.conf`
-    ];
-    // Execute each command one by one
-    for (const command of commands) {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          // Add a command to remove synthetic.conf in case of failure
-          exec('sudo rm /etc/synthetic.conf', (rmError) => {
-              console.log('synthetic.conf removed');
-          });
-          return;
-        }
-        console.log(`Command executed successfully: ${command}`);
-        if (stdout) console.log(`Command output:\n${stdout}`);
-        if (stderr) console.error(`Command error:\n${stderr}`);
-      });
+  try {
+    // Create an Azure CLI credential
+    const credential = new AzureCliCredential();
+
+    // Create a SecretClient
+    const vaultUrl = `https://${vaultName}.vault.azure.net/`;
+    const client = new SecretClient(vaultUrl, credential);
+
+    // Get all secrets from the Key Vault
+    const secrets = client.listPropertiesOfSecrets();
+
+    // Create the output directory if it doesn't exist
+    if (outputDir) {
+      fs.mkdirSync(path.join(outputDir, nameSpace), { recursive: true });
+    }
+
+    // Loop through the secrets and store them in files
+    for await (const secretProperties of secrets) {
+      const secretName = secretProperties.name;
+      const secretValue = await client.getSecret(secretName);
+
+      // Define the output file path
+      const filePath = outputDir ? path.join(outputDir, nameSpace, secretName) : secretName;
+
+      // Write the secret value to the file
+      fs.writeFileSync(filePath, secretValue.value);
+
+      console.log(`Secret '${secretName}' stored in '${filePath}'`);
+    }
+  } catch (error) {
+    if (error.name === 'CredentialUnavailableError') {
+      console.error("Azure CLI credential is not available. Please log in using 'az login'.");
+      program.help();
+    } else {
+      console.error(error);
+      program.help();
     }
   }
+}
+
+async function createSyntheticLink(folderName, actualPath) {
+  const commands = [
+    `sudo touch /etc/synthetic.conf`,
+    `sudo chmod 0666 /etc/synthetic.conf`,
+    `sudo printf "${folderName}\t${actualPath}\n" >> /etc/synthetic.conf`,
+    `sudo chmod 0644 /etc/synthetic.conf`,
+    `sudo chown root:wheel /etc/synthetic.conf`,
+    `/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util -t`,
+    `sudo rm /etc/synthetic.conf`
+  ];
+  // Execute each command one by one
+  for (const command of commands) {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        // Add a command to remove synthetic.conf in case of failure
+        exec('sudo rm /etc/synthetic.conf', (rmError) => {
+            console.log('synthetic.conf removed');
+        });
+        return;
+      }
+      console.log(`Command executed successfully: ${command}`);
+      if (stdout) console.log(`Command output:\n${stdout}`);
+      if (stderr) console.error(`Command error:\n${stderr}`);
+    });
+  }
+}
 
 async function run(){
-    
     //download secrect into the mount location
     await mountSecrets(`${namespace}-${environment}`, namespace, mount);
-
     if(isCatalinaOrLater && mount === `/${CatalinaMount}/secrets`){
       if(!fs.existsSync(syntheticPath)){
           console.log('Due to versions of Catalina OS and upwards only allowing read only /mnt cannot be created. We will now create a synthetic link to /Volumes/mnt');
@@ -128,7 +126,6 @@ async function run(){
           return;
       }
     }
-    
 }
 
 run();
